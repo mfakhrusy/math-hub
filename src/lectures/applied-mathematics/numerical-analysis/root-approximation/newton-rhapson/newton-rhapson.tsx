242edfabc00402f91e6c80a8276443362a3da327
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2021-01-07 08:07:40 +0700

    add dynamic math expression visualizer

--- src/lectures/applied-mathematics/numerical-analysis/root-approximation/newton-rhapson/newton-rhapson.tsx
+++ src/lectures/applied-mathematics/numerical-analysis/root-approximation/newton-rhapson/newton-rhapson.tsx
@@ -2,11 +2,12 @@ import { compile } from 'mathjs';
 import dynamic from 'next/dynamic';
 import { ReactMarkdown } from '@/components/ReactMarkdown';
 import { Spacer } from '@/components/Spacer';
-import { Flex, Heading, Input, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading, Input, Text } from '@chakra-ui/react';
 import { scaleLinear } from '@visx/scale';
-import { Bar, LinePath } from '@visx/shape';
+import { LinePath } from '@visx/shape';
 import { curveNatural } from '@visx/curve';
 import { range } from 'mathjs';
+import { useState } from 'react';
 
 const Introduction = dynamic({
   loader: async () => {
@@ -18,13 +19,27 @@ const Introduction = dynamic({
   }
 });
 
+type Data = {
+  x: number;
+  y: number;
+}
+
 const xSet = range(-2, 2.1, 0.1).toArray() as Array<number>;
 
-const baseFunc = 'x^2 - 4';
+const baseFunc = 'x - 4';
 
-const expr = compile(baseFunc);
+const calculateExpr = (func: string) => compile(func);
 
-const ySet: Array<number> = xSet.map((x: number) => expr.evaluate({x: x}))
+const getX = (d: Data) => d.x;
+const getY = (d: Data) => d.y;
+
+export default function NewtonRhapson() {
+  const [inputFunc, setInputFunc] = useState<string>(baseFunc);
+  const [func, setFunc] = useState<string>(baseFunc);
+  const width = 200;
+  const height = 400;
+
+const ySet: Array<number> = xSet.map((x: number) => calculateExpr(func).evaluate({x: x}))
 
 const xScale = scaleLinear<number>({
   domain: [-2, 3],
@@ -34,28 +49,13 @@ const yScale = scaleLinear<number>({
   domain: [-4, 4],
 });
 
-type Data = {
-  x: number;
-  y: number;
-}
-
 const data: Array<Data> = xSet.map((x: number, i) => ({
   x: x,
   y: ySet[i]
 }));
 
-const getX = (d: Data) => d.x;
-const getY = (d: Data) => d.y;
-
-export default function NewtonRhapson() {
-  const width = 100;
-  const height = 200;
-
   xScale.range([0, width - 50]);
   yScale.range([height - 2, 0]);
-
-
-  console.log(data);
 
   return (
     <Flex width="100%" flexDirection="column">
@@ -66,15 +66,19 @@ export default function NewtonRhapson() {
           <Heading size="md">Enter math expression below!</Heading>
           <Text>{`example: ${baseFunc}`}</Text>
           <Spacer height={5} />
-          <Input variant="flushed" />
+          <Flex>
+            <Input value={inputFunc} onChange={(e) => setInputFunc(e.target.value)} variant="flushed" width="200px" />
+          <Spacer width={5} />
+            <Button onClick={() => setFunc(inputFunc)}>Ok!</Button>
+          </Flex>
         </Flex>
         <Flex width="50%" backgroundColor="white">
-          <svg width="1000" height="1000">
+          <svg width="1000" height="500">
             <LinePath
               height="100"
               curve={curveNatural}
               data={data}
-              x={d => {console.log(d); return xScale(getX(d)) ?? 0}}
+              x={d => xScale(getX(d)) ?? 0}
               y={d => yScale(getY(d)) ?? 0}
               stroke="#333"
               strokeWidth={2}
