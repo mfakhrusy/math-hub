f05a6f0e00f06a6a0f8994f5c6125214e73508ee
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2021-01-08 09:34:13 +0700

    comment out wasm

--- src/pages/wasm-example.tsx
+++ src/pages/wasm-example.tsx
@@ -1,27 +1,27 @@
-import dynamic from "next/dynamic";
-import wasmModule from "../wasm/build/bind";
+// import dynamic from "next/dynamic";
+// import wasmModule from "../wasm/build/bind";
 
-interface Props {
-  firstNumber: number;
-  lastNumber: number;
-}
+// interface Props {
+//   firstNumber: number;
+//   lastNumber: number;
+// }
 
-const Add = dynamic<Props>({
-  loader: async () => {
-    const wasm = await wasmModule();
+// const Add = dynamic<Props>({
+//   loader: async () => {
+//     const wasm = await wasmModule();
 
-    return ({ firstNumber, lastNumber }) => {
-      return (
-        <div>{`lerp: ${wasm.lerp(3, 4, 4)}; multiply: ${wasm.multiply(
-          firstNumber,
-          lastNumber
-        )}`}</div>
-      );
-    };
-  },
-  ssr: false,
-});
+//     return ({ firstNumber, lastNumber }) => {
+//       return (
+//         <div>{`lerp: ${wasm.lerp(3, 4, 4)}; multiply: ${wasm.multiply(
+//           firstNumber,
+//           lastNumber
+//         )}`}</div>
+//       );
+//     };
+//   },
+//   ssr: false,
+// });
 
-export default function Index() {
-  return <Add firstNumber={1} lastNumber={300} />;
-}
+// export default function Index() {
+//   return <Add firstNumber={1} lastNumber={300} />;
+// }
