020d199c01eb719e3d5d1e51a15cedb8115e308b
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2021-01-14 02:44:20 +0700

    add error handling if invalid math expression is filled

--- src/modules/equationVisualizer/components/EquationVisualizerView.tsx
+++ src/modules/equationVisualizer/components/EquationVisualizerView.tsx
@@ -3,12 +3,18 @@ import { DataRange, GraphToolsLayout } from "@/modules/tools/graph";
 import { EquationVisualizerHeader } from "./EquationVisualizerHeader";
 import { useEquationVisualizerStore } from "../equationVisualizerStore";
 import { useGraphStore } from "@/modules/tools/graph/graphStore";
+import { useToast } from "@chakra-ui/react";
+import { useRef } from "react";
 
-const calculateExpr = (func: string) => compile(func);
+const calculateExpr = (func: string) => {
+  return compile(func);
+};
 
 export function EquationVisualizerView() {
   const equationVisualizerStore = useEquationVisualizerStore();
   const graphStore = useGraphStore();
+  const toast = useToast();
+  const errorToastRef = useRef<string | number | undefined>();
 
   const xDataRange = range(
     graphStore.axisRange.x.min,
@@ -16,9 +22,23 @@ export function EquationVisualizerView() {
     0.1
   ).toArray() as Array<number>;
 
-  const yDataRange: Array<number> = xDataRange.map((x: number) =>
-    calculateExpr(equationVisualizerStore.displayedFunc).evaluate({ x: x })
-  );
+  let yDataRange: Array<number> = [];
+
+  try {
+    yDataRange = xDataRange.map((x: number) =>
+      calculateExpr(equationVisualizerStore.displayedFunc)?.evaluate({ x: x })
+    );
+  } catch (err) {
+    if (errorToastRef.current) {
+      toast.close(errorToastRef.current);
+    }
+    errorToastRef.current = toast({
+      status: "error",
+      title: `Error: '${equationVisualizerStore.displayedFunc}' is not a valid math expression`,
+      position: "top",
+    });
+    equationVisualizerStore.setDisplayedFunc("");
+  }
 
   const dataRange: DataRange = xDataRange.map((x: number, i) => ({
     x: x,
